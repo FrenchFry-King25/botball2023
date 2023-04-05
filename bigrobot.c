01b579c9ee30258f13616c0229d77d7a72c5d834
FrenchFry-King25 <68553646+FrenchFry-King25@users.noreply.github.com>
2023-04-05 17:14:13 -0400

    Co-authored-by: glololes <glololes@users.noreply.github.com>

--- bigrobot.c
+++ bigrobot.c
@@ -12,9 +12,9 @@ const int CLAW_CLOSE_POSITION = 0;
 const int CLAW_DELAY = 100;
 
 // DRIVING CONSTANTS
-const double INCHES_CONSTANT = 1.00;
 const int TAPE_THRESHOLD = 1800;
 const int TAPE_SPEED = 250 ; // mm per second, should be low
+const double ROTATION_CONSTANT = 27 / 90;
 
 // ARM CONSTANTS
 const int TOWER_RIGHT_MAX = 1800; // putting rings in towers
@@ -22,12 +22,13 @@ const int RIGHT_ARM_MAX = 20;
 const int LEFT_ARM_MIN = 20; // collecting rings
 const int RIGHT_ARM_MIN = 1800;
 
+
 int main()
 {
     enable_servos();
     create_connect();
     
-    robotForward(500);
+    robotMove(100);
 
     create_disconnect();
     disable_servos();
@@ -37,6 +38,7 @@ int main()
     return 0;
 }
 
+
 void initServoTowers()
 {
     set_servo_position(TOWER_RIGHT, 0);
@@ -45,34 +47,19 @@ void initServoTowers()
     set_servo_position(CLAW, 1023);
 }
 
-void putRingIndex(int ringIndex)
-{
-    /*
-    ringIndex :
+//Movement
 
-    0 - red ring
-    1 - orange ring
-    2 - yellow ring
-    3 - green ring
-    4 - blue ring
-    */
-}
-
-
-
-void robotForward(int i)
-{
+void robotMove(int i){
     set_create_distance(0);
-    
-    printf("%lf", (i * INCHES_CONSTANT));
-    
-    while (get_create_distance() < (i * INCHES_CONSTANT))
-    {
-        create_drive_direct(100, 100);
-        printf("%i", get_create_distance());
+    if(i > 0){
+        while (-1 * get_create_distance() < (i)){
+            create_drive_direct(200, 200);
+        }
+    }else{
+        while(get_create_distance() < (i)){
+            create_drive_direct(-200, -200);
+        }
     }
-    
-    printf("%i", get_create_distance());
     create_stop();
 }
 
@@ -108,82 +95,91 @@ void FromNthTowerToMthTower(int n, int m){
 }
 
 void forwardTillBump() {
-    while ((get_create_distance() < 1000) && (get_create_rbump() == 0)) {
+    while (get_create_rbump() == 0) {
         create_drive_direct(200, 200);
     } 
 }
 
-void turnLeft(int degrees)
-{
-    set_create_total_angle(0);
-    while (get_create_total_angle() < degrees)
-    {
-        create_drive_direct(-100, 100);
-    }
-    create_stop();
-}
 
-void turnRight(int degrees)
-{
-    set_create_total_angle(0);
-    while (get_create_total_angle() > (degrees))
-    {
-        create_drive_direct(100, -100);
-    }
-
-     create_stop();
-}
 
 void turnAround(){
-    turnRight(90);
-    turnRight(90);
-}
-
-void openClaw()
-{
-    set_servo_position(CLAW, CLAW_OPEN_POSITION);
-    msleep(CLAW_DELAY);
-}
-
-void closeClaw()
-{
-    set_servo_position(CLAW, CLAW_CLOSE_POSITION);
-    msleep(CLAW_DELAY);
+    robotTurn(90);
+    robotTurn(90);
 }
 
 
 void boxUnderPomPom(){
-    //
-    robotForward(5); //or some integer
-    turnRight(90);
+    robotMove(5); //forward to push the box under the pom pom
+    robotTurn(90);
 
-    robotForward(5);
-    
-    turnLeft(90);
-
-    forwardTillBump();
-
-    turnLeft(90);
-
-    forwardTillBump();
-
-    turnAround();
+    robotMove(5);
 }
 
-void yeetBotGirl(){
+void getBotGirl(){
     FromNthTowerToMthTower(1, 3);
-     
     //arm motion
 }
 
-void liftArm() {
-    set_servo_position(TOWER_RIGHT, 0);
-    set_servo_position(TOWER_LEFT, 100);
-    msleep(1000);
-}
 
-void lowerArm(){
-    set_servo_position(TOWER_RIGHT, 100);
-    set_servo_position(TOWER_LEFT, 0);
-    msleep(1000);
-}
+
+
+//Rings : 
+// void liftArm() {
+//     set_servo_position(TOWER_RIGHT, 0);
+//     set_servo_position(TOWER_LEFT, 100);
+//     msleep(1000);
+// }
+
+// void lowerArm(){
+//     set_servo_position(TOWER_RIGHT, 100);
+//     set_servo_position(TOWER_LEFT, 0);
+//     msleep(1000);
+// }
+
+// void doRing(int ringIndex)
+// {
+
+//     /*
+//     ringIndex 
+//     0 - red ring
+//     1 - orange ring
+//     2 - yellow ring
+//     3 - green ring
+//     4 - blue ring
+//     */
+
+//    enable_servos();
+   
+//    setClawYRingPosition(ringIndex);
+//    setClawParallelPosition(ringIndex);
+//    setClawClosePosition(ringIndex);
+
+//    disable_servos();
+// }
+
+// void openClaw()
+// {
+//     set_servo_position(CLAW, CLAW_OPEN_POSITION);
+//     msleep(CLAW_DELAY);
+// }
+
+// void setClawClosePosition(int ringIndex)
+// {
+//     int ClawClosePositions[5] = {};
+//     set_servo_position(CLAW, ClawClosePositions[ringIndex]); 
+//     msleep(1000);
+// }
+// void setClawYRingPosition(int ringIndex){
+//     int ClawYRingPositions[5] = {1361, 1431, 1522, 1540, 1616};
+//     set_servo_position(TOWER_RIGHT, ClawYPositions[ringIndex]);
+//     msleep(1000);
+// }
+// void setClawYTowerPosition(int ringIndex){
+//     int ClawYTowerPositions[5] = {};
+//     set_servo_position(TOWER_LEFT, ClawYTowerPositions[ringIndex]);
+// }
+// void setClawParallelPosition(int ringIndex){
+//     int ClawParallelPosition[5] = {123, 123, 0, 0, 0};
+//     set_servo_position(CLAW_PARLLEL, ClawParallelPosition[ringIndex]);
+//     msleep(1000);
+// }
