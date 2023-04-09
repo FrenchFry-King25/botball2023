708b7c0ea1138ee791f37737136178e03a2f15d8
Devin Park <devinpark345@gmail.com>
2023-04-09 17:59:14 -0400

    commmit for big robot code 4/9/2023

--- bigrobot.c
+++ bigrobot.c
@@ -3,14 +3,20 @@
 // DRIVING CONSTANTS
 const int TAPE_THRESHOLD = 1800;
 const int TAPE_SPEED = 250; // mm per second, should be low
-const double ROTATION_CONSTANT = 27 / 90;
+const double ROTATION_CONSTANT = 27.0 / 90.0;
 
 int main()
 {
     enable_servos();
     create_connect();
 
-    initServos();
+    int i = 0;
+    while (i == 0)
+    {
+        printf("%i", get_create_lfcliff_amt());
+        printf(",");
+        msleep(250);
+    }
 
     create_disconnect();
     disable_servos();
@@ -70,8 +76,12 @@ void FromNthTowerToMthTower(int n, int m)
     int i = n;
     while (i <= m)
     {
+        printf("%i", get_create_lfcliff_amt());
+        printf(",");
+        printf("%i", TAPE_THRESHOLD);
         while ((get_create_lfcliff_amt() < TAPE_THRESHOLD))
         {
+            printf("%i", get_create_lfcliff_amt());
             create_drive_direct(TAPE_SPEED, TAPE_SPEED);
         }
 
@@ -92,6 +102,22 @@ void forwardTillBump()
 
 void robotTurn(int angle)
 {
+    set_create_total_angle(0);
+
+    if (angle < 0)
+    {
+        while (-1 * get_create_total_angle() > angle * ROTATION_CONSTANT)
+        {
+            create_drive_direct(-200, 200);
+        }
+    }
+    else
+    {
+        while (-1 * get_create_total_angle() < angle * ROTATION_CONSTANT)
+        {
+            create_drive_direct(200, -200);
+        }
+    }
 }
 void turnAround()
 {
@@ -104,8 +130,11 @@ void turnAround()
 // SERVO PINS
 const int CLAW = 0;         // powering the claw to open and close
 const int CLAW_PARLLEL = 1; // the vertical rotation of the servo for the sake of making the claw move parallel-ly
-const int TOWER_LEFT = 2;   // this servo is used to drop off the rings on the TOWER
-const int TOWER_RIGHT = 3;  // this servo is used to pick up the rings from the FLOOR (tower left and right do different things b/c we want it to go more than 180 degrees)
+// MOTOR PINS
+const int ORANGE_MOTOR = 0; // this is the motor on the LEFT
+const int BLACK_MOTOR = 1;  // this is the motor on the RIGHT
+// MOTOR RATIOS
+const int MOTOR_ROATION_CONSTANT = 1;
 
 /* ringIndex :
 0 - RED
@@ -121,14 +150,6 @@ void initServos()
     msleep(1000);
 
     clawParallel(0);
-    msleep(1000);
-
-    disable_servo(TOWER_RIGHT);
-    msleep(100);
-    set_servo_position(TOWER_LEFT, 0);
-    msleep(1000);
-
-    clawPickup(0);
     msleep(1000);
 }
 
@@ -148,51 +169,42 @@ void clawParallel(int ringIndex)
     set_servo_position(CLAW_PARLLEL, clawParallelPositionList[ringIndex]);
 }
 
-const int clawPickupPositionList[5] = {1260, 1295, 1380, 1410, 1500};
-void clawPickup(int ringIndex)
+const int motorBottomTickList[5] = {750, 0, 0, 0, 0}; // in ticks (aka. position) this is for picking up the rings
+void motorMoveRingBottom(int ringIndex)
 {
-    // The right servo does this
-    disable_servo(TOWER_LEFT); // disable to prevent any breaking of servos
-    msleep(100);
-    enable_servo(TOWER_RIGHT); // enable because it might potentionally be disabled
-    msleep(100);
-    set_servo_position(TOWER_RIGHT, clawPickupPositionList[ringIndex]);
-}
+    cmpc(ORANGE_MOTOR);
+    cmpc(BLACK_MOTOR);
 
-void int clawDropOffPositionList[5] = {0, 0, 0, 0, 0};
-void clawDropOff(int ringIndex)
-{
-    disable_servo(TOWER_RIGHT);
-    msleep(100);
-    enable_servo(TOWER_LEFT);
-    msleep(100);
-    set_servo_position(TOWER_LEFT, clawDropOffPositionList[ringIndex]);
+    while (gmpc(ORANGE_MOTOR) < (motorBottomTickList[ringIndex]))
+    {
+        motor(ORANGE_MOTOR, 75);
+        if (gmpc(BLACK_MOTOR) < gmpc(ORANGE_MOTOR))
+        {
+            motor(BLACK_MOTOR, 100);
+        }
+        else
+        {
+            motor(BLACK_MOTOR, 50);
+        }
+    }
 }
 
 void doRingBottom(int ringIndex) // pick up the rings FROM the (bottom)
 {
-    clawOpen();
-    msleep(1000);
-    clawPickup(ringIndex);
-    msleep(1000);
-    clawParallel(ringIndex);
-    msleep(1000);
-    closeClaw(ringIndex);
-    msleep(1000);
 }
 
 /*--------------------------------------------------------Complex Movement--------------------------------------------------------*/
 
 void boxUnderPomPom()
 {
-    robotMove(5); // forward to push the box under the pom pom
     robotTurn(90);
+    robotMove(20);
 
-    robotMove(5);
+    robotTurn(-90);
+    forwardTillBump();
 }
 
 void getBotGirl()
 {
     FromNthTowerToMthTower(1, 3);
-    // arm motion
 }
